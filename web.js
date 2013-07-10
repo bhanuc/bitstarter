f77e47dc95952a4d008ba72955862dfbf8f6151a
bhanuc <bhanuc@iitk.ac.in>
2013-07-11 04:21:02 +0530

    h

--- web.js
+++ web.js
@@ -1,19 +1,15 @@
 var express = require('express');
 var app = express.createServer(express.logger());
-var fs = require('fs');
 
-fs.readFile('./index.html', function(err, html) {
-    if(err) {
-	throw err;
-    }
+
+
 
 app.get('/', function(request, response) {
-    response.write(html);
+    response.render("index.html");
 });
 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
 });
