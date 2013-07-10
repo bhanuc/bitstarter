b7154826249cea86d478fc240dc5b437f7792494
bhanuc <bhanuc@iitk.ac.in>
2013-07-11 00:04:05 +0530

    update

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var ap[]= ;
+fs.readFile('index.html',function (err, data) {
+    if (err) throw err;
+    ap = data ;
+});
+    
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(ap);
 });
 
 var port = process.env.PORT || 5000;
